3cb047a646941e47e9178ea233684211c1fb2186
DaZhuXiaoYu <48450964@qq.com>
2020-01-16 19:39:23 +0800

    第二次提交

--- tomato/src/main/java/dao/User.java
+++ tomato/src/main/java/dao/User.java
@@ -1,4 +1,14 @@
 package dao;
 
+import java.util.UUID;
+
 public class User {
+    public static void main(String[] args) {
+        for( int i = 0; i< 10; i++){
+            UUID uuid = UUID.randomUUID();
+            String s = uuid.toString();
+            System.out.println(s);
+            System.out.println(s.length());
+        }
+    }
 }
